148b41ffd2a69a498b83e16eeed873258058328a
samssi <samir.hamadi@gmail.com>
2023-04-13 20:04:44 +0000

    Make migration file reader

---
+++ src/output/log.ts
@@ -0,0 +1,11 @@
+export const debug = (message?: any) => {
+    console.log(message)
+}
+
+export const info = (message?: any) => {
+    console.log(message)
+}
+
+export const error = (message?: any) => {
+    console.log(message)
+}
