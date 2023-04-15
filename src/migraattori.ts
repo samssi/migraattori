ab2ebe62c11216ea44124a1246c4894a711f3fb2
samssi <samir.hamadi@gmail.com>
2023-04-15 19:10:56 +0000

    Modularize database connection

--- src/migraattori.ts
+++ src/migraattori.ts
@@ -1,21 +1,34 @@
 import {sql} from "./db/postgresDb";
 import {readSqlFiles, SQLFile} from "./input/fileReader";
 
-const migrationTransaction = async (sqlFiles: SQLFile[]) => {
-    await sql.begin(async sql => {
-        for (const sqlFile of sqlFiles) {
-            await sql.unsafe(sqlFile.content)
-        }
-    })
+interface TargetDatabaseDriver {
+    migrationTransaction(sqlFiles: SQLFile[]): Promise<void>
+    closeConnection(): Promise<void>
 }
 
+const postgresJs: TargetDatabaseDriver = {
+    async migrationTransaction(sqlFiles: SQLFile[]): Promise<void> {
+        await sql.begin(async sql => {
+            for (const sqlFile of sqlFiles) {
+                await sql.unsafe(sqlFile.content)
+            }
+        })
+        await sql.end()
+    },
+    async closeConnection(): Promise<void> {
+        await sql.end()
+    }
+}
 
+const runMigrations = async (driver: TargetDatabaseDriver) => {
+    const sqlFiles = await readSqlFiles("sql")
+    await driver.migrationTransaction(sqlFiles)
+    await driver.closeConnection()
+}
 
 const main = async () => {
     console.log("Migraattori starting...")
-    const sqlFiles = await readSqlFiles("sql")
-    await migrationTransaction(sqlFiles)
-    await sql.end()
+    await runMigrations(postgresJs)
     console.log("Migraattori executed successfully...")
 }
 
