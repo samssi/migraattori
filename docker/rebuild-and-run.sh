25fc6c768a925b55845a0bb49866e4c233d4b5d9
samssi <samir.hamadi@gmail.com>
2023-04-15 18:10:03 +0000

    Add and update scripts for easier/faster development time with command line commands

--- docker/rebuild-and-run.sh
+++ docker/rebuild-and-run.sh
@@ -1,6 +1,6 @@
 CONTAINER_NAME=migraattori-test-postgres
 
 docker rm -f $CONTAINER_NAME
-docker build -t $CONTAINER_NAME .
+docker build -t $CONTAINER_NAME docker/
 docker run -d -p 5432:5432 --name $CONTAINER_NAME $CONTAINER_NAME
 # docker logs -f $CONTAINER_NAME
